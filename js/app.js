4f922e078605223561ddd0a186854d65b90e4241
аппле <apple@MacBook-Air-apple.local>
2024-09-29 23:08:45 +0500

    new javascript file add

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// Ready
+
+// // 1-masala
+// let num = +prompt ("Sonni kiriting") ;
+
+// if(num > 0) {
+//  num = num + 1
+// } else {
+//     num = num -1
+// }
+// console.log(num)
+
+// 2-masala
+let number = +prompt("Sopnni kiriting") ;
+if(number>0){
+    number = number + 1
+} else {
+    number = number -2
+}
+console.log(number);
+
+
+
+
+
